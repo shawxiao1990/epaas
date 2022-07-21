8dfecceabb98a65c8ad802db87d1f80f1934a792
肖冰君 <bingjunxiao@emotibot.com>
2022-07-21 11:54:06 +0800

    add post api /server/create

--- auth/apis/v1/resources.py
+++ auth/apis/v1/resources.py
@@ -10,6 +10,7 @@ from auth.extensions import db
 from auth.models import User
 from flask import current_app
 import logging
+import demjson
 
 
 def get_item_body():
@@ -91,8 +92,42 @@ class EndpointAPI(MethodView):
         })
 
 
+class ServerAPI(MethodView):
+    decorators = [auth_required]
+
+    def post(self):
+        # data = json.loads(data, strict=False)
+        # print(data)
+        raw_data = request.get_data()
+        data = demjson.decode(raw_data)
+
+        ip = data.get('serverip')
+        name = data.get('servername')
+        endpoint = data.get('endpoint')
+        roles = data.get('roles')
+
+        from auth.models import Endpoint, Server
+        server = Server(
+            name=name,
+            ip=ip,
+            roles=roles,
+            endpoints=[Endpoint.query.filter_by(name=endpoint).first()]
+        )
+        db.session.add(server)
+        db.session.commit()
+
+        response = jsonify({
+            'data': 'succeed',
+            'code': 20000
+        })
+        response.headers['Cache-Control'] = 'no-store'
+        response.headers['Pragma'] = 'no-cache'
+        return response
+
+
 api_v1.add_url_rule('/', view_func=IndexAPI.as_view('index'), methods=['GET'])
 api_v1.add_url_rule('/oauth/token', view_func=AuthTokenAPI.as_view('token'), methods=['POST'])
 api_v1.add_url_rule('/user', view_func=UserAPI.as_view('user'), methods=['GET'])
 api_v1.add_url_rule('/role', view_func=RoleAPI.as_view('role'), methods=['GET'])
 api_v1.add_url_rule('/server/endpoint', view_func=EndpointAPI.as_view('endpoint'), methods=['GET'])
+api_v1.add_url_rule('/server/create', view_func=ServerAPI.as_view('create'), methods=['POST'])
