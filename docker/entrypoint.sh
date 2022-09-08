040ad34b87aaad5f45c23267da6a5093b4c5aa3b
肖冰君 <bingjunxiao@emotibot.com>
2022-09-08 18:53:39 +0800

    feat: flask init

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,1 +1,17 @@
-pipenv run gunicorn --workers=1 --threads=1 --bind=0.0.0.0:5000 --timeout 1800 'epaas:create_app()'
+#!/bin/sh
+set -e
+
+envsubst '
+$$FLASK_CONFIG
+$$FLASK_APP
+' < /app/env.template > /app/.env
+
+pipenv run flask init
+
+#pipenv run flask db init
+#
+#pipenv run flask db migrate -m "test"
+#
+#pipenv run flask db upgrade
+
+pipenv run gunicorn --workers=1 --threads=1 --bind=0.0.0.0:${PORT} --timeout 1800 '${FLASK_APP}:create_app()'
