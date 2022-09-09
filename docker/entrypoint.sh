128cb09d3e850f810bc40aa477af434568fd6ef8
肖冰君 <bingjunxiao@emotibot.com>
2022-09-09 13:12:41 +0800

    update migration script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,12 +6,14 @@ $$FLASK_CONFIG
 $$FLASK_APP
 ' < /app/env.template > /app/.env
 
-pipenv run flask init
+if [[ ! -f migrations/versions/*.py ]]; then
+    pipenv run flask init
+else
+    #pipenv run flask db init
 
-#pipenv run flask db init
-#
-#pipenv run flask db migrate -m "test"
-#
-#pipenv run flask db upgrade
+    #pipenv run flask db migrate -m "init-epaas"
+
+    pipenv run flask db upgrade
+fi
 
 pipenv run gunicorn --workers=1 --threads=1 --bind=0.0.0.0:${PORT} --timeout 1800 '${FLASK_APP}:create_app()'
